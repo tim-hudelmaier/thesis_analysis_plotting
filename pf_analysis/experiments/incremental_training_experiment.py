cb82e93e768414b3b1aa835792175408b9201eaf
tim-hudelmaier <tim.hudelmaier@stud.uni-heidelberg.de>
2023-06-05 18:26:54 +0200

    fix remaining bugs in incremental_training_experiment.py

--- pf_analysis/experiments/incremental_training_experiment.py
+++ pf_analysis/experiments/incremental_training_experiment.py
@@ -1,6 +1,12 @@
+import json
 import multiprocessing as mp
 from pathlib import Path
 from uuid import uuid4
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+import pandas as pd
 
 import peptide_forest
 
@@ -19,4 +25,33 @@ if __name__ == "__main__":
     )
     pf.boost(write_results=False, dump_train_test_data=True)
 
+    tt_data_dir = DATA_DIR / "experiment_outputs" / "tt_data"
+    model_dir = DATA_DIR / "experiment_outputs" / "models"
 
+    results = []
+    for file in tt_data_dir.glob("*.json"):
+        fold = int(str(file.stem).split("_")[-1][-1])
+        engine_paths = model_dir.glob(f"*f{fold}.json")
+
+        for engine_path in engine_paths:
+            epoch = int(str(engine_path.stem).split("_")[-2][-1])
+            spectra = [s for s in json.load(open(file, "r"))["test_spectra"]]
+            pf.engine = peptide_forest.training.get_classifier()
+            pf.engine.load_model(engine_path)
+            eval_gen = pf.get_data_chunk(mode="spectrum", reference_spectra=spectra)
+            df = pf.get_results(gen=eval_gen, use_disk=False, write_output=False)
+            n_psms = df["top_target_peptide_forest"].sum()
+            results.append((fold, epoch, n_psms))
+
+    plt_df = pd.DataFrame(results, columns=["fold", "epoch", "n_psms"])
+    plt_df = plt_df.groupby("epoch").sum().drop(columns=["fold"]).reset_index()
+
+    sns.set(style="whitegrid")
+
+    plt.figure(figsize=(10, 6))
+    sns.lineplot(x=plt_df["epoch"], y=plt_df["n_psms"], marker="o", color="black")
+
+    plt.xlabel("Iteration", fontsize=14)
+    plt.ylabel("Total PSMs", fontsize=14)
+    plt.title("Total PSMs per Iteration", fontsize=16)
+    plt.show()
