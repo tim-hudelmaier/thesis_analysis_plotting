f44873742feb2b4be40db81483ebd2137e3d7355
tim-hudelmaier <tim.hudelmaier@stud.uni-heidelberg.de>
2023-06-19 18:18:44 +0200

    chore: black

--- app.py
+++ app.py
@@ -3,15 +3,16 @@ import incremental_training_experiment as ite
 
 app = Flask(__name__)
 
-@app.route('/run_experiment', methods=['POST'])
+
+@app.route("/run_experiment", methods=["POST"])
 def run_experiment():
     config = request.get_json(force=True)
     ite.run(config)
-    return jsonify([{'result': 'success'}])
+    return jsonify([{"result": "success"}])
 
 
-@app.route('/run_single_epoch', methods=['Post'])
+@app.route("/run_single_epoch", methods=["Post"])
 def run_single_epoch():
     config = request.get_json(force=True)
     ite.run_single_epoch(config)
-    return jsonify([{'result': 'success'}])
+    return jsonify([{"result": "success"}])
